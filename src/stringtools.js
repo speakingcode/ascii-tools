d323cb3fd8d70cadb31213932db7760bb18f9c61
speakingcode <dlissner@gmail.com>
2017-11-26 20:57:07 -0600

    Clean up formatting

---
+++ src/stringtools.js
@@ -0,0 +1,17 @@
+const overlayChars = (
+  bottom        = "",
+  top           = "",
+  offset        = 0,
+) => {
+    let bottomLeftEnd     = offset,
+        topEnd            = offset > bottom.length ? 0 : bottom.length - offset,
+        bottomRightStart  = top.length + offset,
+        bottomRightEnd    = offset + top.length > bottom.length ? 0 : bottom.length
+
+    return bottom.slice(0,                bottomLeftEnd) +
+           top.slice   (0,                topEnd) +
+           bottom.slice(bottomRightStart, bottomRightEnd)
+
+}
+
+export { overlayChars }

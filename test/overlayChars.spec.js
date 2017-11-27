b16f99811190b761ef1b287d6fac1068c924d1dc
speakingcode <dlissner@gmail.com>
2017-11-26 20:52:04 -0600

    Rename source files to match modules

--- test/overlayChars.spec.js
+++ test/overlayChars.spec.js
@@ -1,14 +1,14 @@
 import { expect } from 'chai'
-import { overlayChars } from '../src/stringtools'
+import { overlayChars } from '../src/overlayChars'
 
 describe('overlayChars', () => {
   const b = "0123456789",
         t = "abcd"
 
   it('works with offset 0 when top is shorter than bottom', () => {
-    let offset = 0
-
+    let offset  = 0
     let newLine = overlayChars(b, t, offset)
+
     expect(newLine).to.equal("abcd456789")
   })
 
@@ -19,37 +19,40 @@ describe('overlayChars', () => {
         bottom = t
 
     let newLine = overlayChars(bottom, top, offset)
+
     expect(newLine).to.equal("0123")
   })
 
   it('works with offset that is greater than bottom length', () => {
-    let offset = 11
-
+    let offset  = 11
     let newLine = overlayChars(b, t, offset)
+
     expect(newLine).to.equal("0123456789")
   })
 
   it('works with offset when top + offset is shorter than bottom', () => {
-    let offset = 3
-
+    let offset  = 3
     let newLine = overlayChars(b, t, offset)
+
     expect(newLine).to.equal("012abcd789")
   })
 
   it('works with offset when top + offset is longer than bottom', () => {
-    let offset = 7
-
+    let offset  = 7
     let newLine = overlayChars(b, t, offset)
+
     expect(newLine).to.equal("0123456abc")
   })
 
   it('works when only bottom is provided', () => {
     let newLine = overlayChars(b)
+
     expect(newLine).to.equal("0123456789")
   })
 
   it('works with no input', () => {
     let newLine = overlayChars()
+
     expect(newLine).to.equal("")
   })
 })
